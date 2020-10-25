a08dd4b43934727fe2f2f32cd387b38ea56f53b4
Tiago <tiagocabrera14@gmail.com>
2020-10-25 02:39:50 -0300

    Page -Variables-

--- trabajo_integrador/js/navbar.js
+++ trabajo_integrador/js/navbar.js
@@ -4,21 +4,3 @@ function menuToggle() {
   let toggle = document.getElementById("toggle");
   toggle.classList.toggle("active");
 }
-
-var desktop = "Lenovo";
-let notebook = "Dell";
-const allInOne = "HP";
-
-if (desktop) {
-  var desktop = "Asus";
-  console.log(`Adentro del if: ${desktop}`); // Output: Asus
-}
-console.log(`Afuera del if: ${desktop}`); // Output: Asus
-
-if (notebook) {
-  let notebook = "Acer";
-  console.log(`Adentro del if: ${notebook}`); // Output: Acer
-}
-console.log(`Afuera del if: ${notebook}`); // Output: Dell
-
-console.log(`Constante: ${allInOne}`); // Output: HP
